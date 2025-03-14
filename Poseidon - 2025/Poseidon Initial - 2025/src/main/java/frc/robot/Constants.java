30d6eb3058e251aa1aaeae948989a61d25c46969
Andrew Rowley <andrew.rowley160@outlook.com>
2025-03-14 16:40:26 -0400

    Edit robot dimentions in constants

--- Poseidon - 2025/Poseidon Initial - 2025/src/main/java/frc/robot/Constants.java
+++ Poseidon - 2025/Poseidon Initial - 2025/src/main/java/frc/robot/Constants.java
@@ -30,9 +30,9 @@ public final class Constants {
 
     // Chassis configuration
     // Distance between centers of right and left wheels on robot
-    public static final double kTrackWidth = Units.inchesToMeters(19.375);
+    public static final double kTrackWidth = Units.inchesToMeters(29.5);
     // Distance between front and back wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(20.625);
+    public static final double kWheelBase = Units.inchesToMeters(29.5);
 
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
         new Translation2d(kWheelBase / 2, kTrackWidth / 2),
