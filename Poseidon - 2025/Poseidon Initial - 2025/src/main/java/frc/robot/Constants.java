c143a0af6cf899f5fda4c702ee647ec15e25a882
Andrew Rowley <andrew.rowley160@outlook.com>
2025-03-14 17:47:59 -0400

    Modify drive can ids

--- Poseidon - 2025/Poseidon Initial - 2025/src/main/java/frc/robot/Constants.java
+++ Poseidon - 2025/Poseidon Initial - 2025/src/main/java/frc/robot/Constants.java
@@ -47,15 +47,15 @@ public final class Constants {
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 5;
-    public static final int kRearLeftDrivingCanId = 8;
-    public static final int kFrontRightDrivingCanId = 6;
-    public static final int kRearRightDrivingCanId = 7;
+    public static final int kFrontLeftDrivingCanId = 6;
+    public static final int kRearLeftDrivingCanId = 5;
+    public static final int kFrontRightDrivingCanId = 7;
+    public static final int kRearRightDrivingCanId = 8;
 
-    public static final int kFrontLeftTurningCanId = 1;
-    public static final int kRearLeftTurningCanId = 4;
-    public static final int kFrontRightTurningCanId = 2;
-    public static final int kRearRightTurningCanId = 3;
+    public static final int kFrontLeftTurningCanId = 2;
+    public static final int kRearLeftTurningCanId = 1;
+    public static final int kFrontRightTurningCanId = 3;
+    public static final int kRearRightTurningCanId = 4;
 
     public static final boolean kGyroReversed = false;
   }
